875f853b363d61149aa88f3b233cfa1d62541708
Cedric Tiavina <135535086+CedtT198@users.noreply.github.com>
2025-12-29 11:06:40 +0300

    update front
    update the pricing list and the access

--- front/stt-entreprise/src/features/pages/public/Subscription.tsx
+++ front/stt-entreprise/src/features/pages/public/Subscription.tsx
@@ -1,66 +1,63 @@
 function Subscription() {
     return (
         <div>
-            <div className="">
+            <div className="text-center">
                 <h2 className="page-title mb-0">Available subscriptions</h2>
                 <p className="text-muted mb-5">See below the offer that fits better for you</p>
             </div>
+            <div className="text-center">
+            </div>
             <div className="card-deck my-4">
                 <div className="card mb-4 shadow">
-                    <div className="card-body text-center my-4">
-                        <a href="#">
-                        <h3 className="h5 mt-4 mb-0">Basic</h3>
-                        </a>
-                        <p className="text-muted">-</p>
-                        <span className="h1 mb-0">$9.9</span>
-                        <p className="text-muted">month</p>
-                        <ul className="list-unstyled mb-5">
+                    <div className="card-body text-center my-4 d-flex flex-column">
+                        <div className="border-bottom-1">
+                            <p className="h3 mb-0">Free</p>
+                            <p className=" mb-0" style={{ fontSize: 45 }}>$9.9</p>
+                            <p className="text-muted">Monthly</p>
+                        </div>
+                        <ul className="list-unstyled">
                             <li>Lorem ipsum dolor sit amet</li>
-                            <li>Consectetur adipiscing elit</li>
-                            <li>Integer molestie lorem at massa</li>
-                            <li>Eget porttitor lorem</li>
+                            <li>Lorem ipsum dolor sit amet</li>
                         </ul>
-                        <span className="dot dot-lg bg-success"></span>
-                        <span className="text-muted ml-3">Active</span>
-                        <button type="button" className="btn mb-2 btn-primary btn-lg">Ugrade</button>
+                        <div className="py-2 mb-3 mt-auto">
+                            <button className="btn btn-primary w-75" style={{ fontSize: 18 }}>Sign up now</button>
+                        </div>
                     </div>
                 </div> 
                 <div className="card mb-4 shadow">
-                    <div className="card-body text-center my-4">
-                        <a href="#">
-                        <h3 className="h5 mt-4 mb-0">Basic</h3>
-                        </a>
-                        <p className="text-muted">-</p>
-                        <span className="h1 mb-0">$9.9</span>
-                        <p className="text-muted">month</p>
-                        <ul className="list-unstyled mb-5">
+                    <div className="card-body text-center my-4 d-flex flex-column">
+                        <div className="border-bottom-1">
+                            <p className="h3 mb-0">Free</p>
+                            <p className=" mb-0" style={{ fontSize: 45 }}>$9.9</p>
+                            <p className="text-muted">Monthly</p>
+                        </div>
+                        <ul className="list-unstyled">
                             <li>Lorem ipsum dolor sit amet</li>
-                            <li>Consectetur adipiscing elit</li>
-                            <li>Integer molestie lorem at massa</li>
-                            <li>Eget porttitor lorem</li>
+                            <li>Lorem ipsum dolor sit amet</li>
+                            <li>Lorem ipsum dolor sit amet</li>
+                            <li>Lorem ipsum dolor sit amet</li>
+                            <li>Lorem ipsum dolor sit amet</li>
+                            <li>Lorem ipsum dolor sit amet</li>
                         </ul>
-                        <span className="dot dot-lg bg-success"></span>
-                        <span className="text-muted ml-3">Active</span>
-                        <button type="button" className="btn mb-2 btn-primary btn-lg">Ugrade</button>
+                        <div className="py-2 mb-3 mt-auto">
+                            <button className="btn btn-primary w-75" style={{ fontSize: 18 }}>Sign up now</button>
+                        </div>
                     </div>
                 </div> 
                 <div className="card mb-4 shadow">
-                    <div className="card-body text-center my-4">
-                        <a href="#">
-                        <h3 className="h5 mt-4 mb-0">Basic</h3>
-                        </a>
-                        <p className="text-muted">-</p>
-                        <span className="h1 mb-0">$9.9</span>
-                        <p className="text-muted">month</p>
-                        <ul className="list-unstyled mb-5">
+                    <div className="card-body text-center my-4 d-flex flex-column">
+                        <div className="border-bottom-1">
+                            <p className="h3 mb-0">Free</p>
+                            <p className=" mb-0" style={{ fontSize: 45 }}>$9.9</p>
+                            <p className="text-muted">Monthly</p>
+                        </div>
+                        <ul className="list-unstyled">
                             <li>Lorem ipsum dolor sit amet</li>
-                            <li>Consectetur adipiscing elit</li>
-                            <li>Integer molestie lorem at massa</li>
-                            <li>Eget porttitor lorem</li>
+                            <li>Lorem ipsum dolor sit amet</li>
                         </ul>
-                        <span className="dot dot-lg bg-success"></span>
-                        <span className="text-muted ml-3">Active</span>
-                        <button type="button" className="btn mb-2 btn-primary btn-lg">Ugrade</button>
+                        <div className="py-2 mb-3 mt-auto">
+                            <button className="btn btn-primary w-75" style={{ fontSize: 18 }}>Sign up now</button>
+                        </div>
                     </div>
                 </div> 
             </div>
